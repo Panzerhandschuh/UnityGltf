2251043ba1d5e9257e8d0865c1c9f8b75d872c94
Panzerhandschuh <TylerBeff@gmail.com>
2018-10-18 21:16:41 -0400

    Added unit tests

---
+++ Assets/Scripts/Gltf/Tests/GltfInterfaceTest.cs
@@ -0,0 +1,15 @@
+﻿using glTFLoader;
+using NUnit.Framework;
+
+namespace UnityGltf.Tests
+{
+	public class GltfInterfaceTest
+	{
+		[TestCase(@"Assets\Scenes\Lantern\Lantern.gltf")]
+		public void CanReadGltfFile(string path)
+		{
+			var gltf = Interface.LoadModel(path);
+			Assert.NotNull(gltf);
+		}
+	}
+}
---
+++ Assets/Scripts/Gltf/Tests/GltfLoaderTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine.TestTools;
+
+namespace UnityGltf.Tests
+{
+	public class GltfLoaderTest
+	{
+		[UnityTest]
+		public IEnumerator CanReadGltfFile()
+		{
+			using (var loader = new GltfLoader(@"Assets\Scenes\Lantern\Lantern.gltf"))
+				loader.Load();
+
+			yield return null;
+		}
+	}
+}
