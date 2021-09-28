e25e7b25dd8a7a16df3b45fae9db44d632f1ec07
Panzerhandschuh <TylerBeff@gmail.com>
2021-09-28 15:18:31 -0600

    Removed default values from unity colliders extension

--- Assets/Scripts/ColliderLoader.cs
+++ Assets/Scripts/ColliderLoader.cs
@@ -34,24 +34,32 @@ namespace UnityGltf
 		private void LoadBoxCollider(Unity_collidersBoxCollider boxCollider, GameObject obj)
 		{
 			var collider = obj.AddComponent<BoxCollider>();
-			collider.center = TypeConverter.ConvertVector3(boxCollider.Center);
-			collider.size = TypeConverter.ConvertVector3(boxCollider.Size, false);
+			if (boxCollider.Center != null)
+				collider.center = TypeConverter.ConvertVector3(boxCollider.Center);
+			if (boxCollider.Size != null)
+				collider.size = TypeConverter.ConvertVector3(boxCollider.Size, false);
 		}
 
 		private void LoadSphereCollider(Unity_collidersSphereCollider sphereCollider, GameObject obj)
 		{
 			var collider = obj.AddComponent<SphereCollider>();
-			collider.center = TypeConverter.ConvertVector3(sphereCollider.Center);
-			collider.radius = sphereCollider.Radius;
+			if (sphereCollider.Center != null)
+				collider.center = TypeConverter.ConvertVector3(sphereCollider.Center);
+			if (sphereCollider.Radius.HasValue)
+				collider.radius = sphereCollider.Radius.Value;
 		}
 
 		private void LoadCapsuleCollider(Unity_collidersCapsuleCollider capsuleCollider, GameObject obj)
 		{
 			var collider = obj.AddComponent<CapsuleCollider>();
-			collider.center = TypeConverter.ConvertVector3(capsuleCollider.Center);
-			collider.radius = capsuleCollider.Radius;
-			collider.height = capsuleCollider.Height;
-			collider.direction = GetDirection(capsuleCollider.Direction);
+			if (capsuleCollider.Center != null)
+				collider.center = TypeConverter.ConvertVector3(capsuleCollider.Center);
+			if (capsuleCollider.Radius.HasValue)
+				collider.radius = capsuleCollider.Radius.Value;
+			if (capsuleCollider.Height.HasValue)
+				collider.height = capsuleCollider.Height.Value;
+			if (capsuleCollider.Direction.HasValue)
+				collider.direction = GetDirection(capsuleCollider.Direction.Value);
 		}
 
 		private int GetDirection(Unity_collidersCapsuleCollider.DirectionEnum direction)
@@ -72,7 +80,8 @@ namespace UnityGltf
 		private void LoadMeshCollider(Unity_collidersMeshCollider meshCollider, GameObject obj)
 		{
 			var collider = obj.AddComponent<MeshCollider>();
-			collider.convex = meshCollider.Convex;
+			if (meshCollider.Convex.HasValue)
+				collider.convex = meshCollider.Convex.Value;
 			if (meshCollider.Mesh.HasValue)
 			{
 				var mesh = meshLoader.LoadMesh(meshCollider.Mesh.Value);
